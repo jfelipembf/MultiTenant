50af300f9ee6b4d8083972aa2c1dd9f4a17f3a49
Felipe Macedo <jfelipembf@gmail.com>
2025-12-11 12:35:30 -0300

    Remove arquivos e pastas de workspace não utilizados

--- src/hooks/data/index.js
+++ src/hooks/data/index.js
@@ -3,6 +3,5 @@ import useDomain from './useDomain';
 import useDomains from './useDomains';
 import useInvitations from './useInvitations';
 import useMembers from './useMembers';
-import useWorkspaces from './useWorkspaces';
 
-export { useBranches, useDomain, useDomains, useInvitations, useMembers, useWorkspaces };
+export { useBranches, useDomain, useDomains, useInvitations, useMembers };
